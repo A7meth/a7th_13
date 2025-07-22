631057604d3fbd352d5eaae6a7232c9872679548
A7TH <122217936+Eslisaje@users.noreply.github.com>
2025-07-22 17:58:07 +0100

    Add files via upload

---
+++ ft_strcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahelamri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/22 16:38:19 by ahelamri          #+#    #+#             */
+/*   Updated: 2025/07/22 17:21:29 by ahelamri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = '\0';
+	while(src[i] != '\0')
+	{
+	dest[i] = src[i];
+	i ++;
+	}
+	return (dest);
+}
+#include <stdio.h>
+
+int	main()
+{
+	char	src[] = "ahmed";
+	char	dest[10];
+	
+	printf("%s\n", ft_strcpy(dest, src));
+}
