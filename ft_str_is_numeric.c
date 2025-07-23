8305c4ef132b60dddb7bbac4d891bb248258b43c
A7TH <122217936+Eslisaje@users.noreply.github.com>
2025-07-23 11:04:08 +0100

    Add files via upload

---
+++ ft_str_is_numeric.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahelamri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 10:55:54 by ahelamri          #+#    #+#             */
+/*   Updated: 2025/07/23 11:03:37 by ahelamri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if(str[i] >= '0' && str[i] <= '9')
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	i ++;
+	}
+	return (i);
+}
+
+#include <stdio.h>
+
+int main()
+{
+	char str[] = "36828462";
+	char str1[] = "dgiwd628325";
+	char str2[] = "dgiwd";
+
+	printf("%d \n", ft_str_is_numeric(str));
+	printf("%d \n", ft_str_is_numeric(str1));
+	printf("%d \n", ft_str_is_numeric(str2));
+}
