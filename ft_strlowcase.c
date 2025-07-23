6f8df0bb4b37724edf1239c1e1ec657567448403
A7TH <122217936+A7meth@users.noreply.github.com>
2025-07-23 14:50:55 +0100

    Add files via upload

---
+++ ft_strlowcase.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahelamri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 14:43:42 by ahelamri          #+#    #+#             */
+/*   Updated: 2025/07/23 14:50:38 by ahelamri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	while (*str)
+	{
+		if(*str >= 'A' && *str <= 'Z')
+			*str += 32;
+	*str ++;
+	}
+	return (str);
+}
+
+#include <stdio.h>
+
+int	main()
+{
+	char	str[] ="AHMED";
+	*ft_strlowcase(str);
+	printf("%s\n",str);
+}
