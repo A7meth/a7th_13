a7f14590b7a3eeaa54a0aba00e194dcb0b3d0b3b
A7TH <122217936+A7meth@users.noreply.github.com>
2025-07-23 12:10:06 +0100

    Add files via upload

---
+++ ft_str_is_uppercase.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahelamri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 12:01:09 by ahelamri          #+#    #+#             */
+/*   Updated: 2025/07/23 12:09:15 by ahelamri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_uppercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i])
+	{
+		if(str[i] >= 'a' && str[i] <= 'z')
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+		i ++;
+	}
+	return (i);
+}
+
+#include <stdio.h>
+
+int	main ()
+{
+	char str[] = "gsfjsgj";
+	char str1[] = "WEQEEfdf";
+	char str2[] = "QRE@#$@356ryhe";
+	printf("%d\n",ft_str_is_uppercase(str));
+	printf("%d\n",ft_str_is_uppercase(str1));
+	printf("%d\n",ft_str_is_uppercase(str2));
+}
