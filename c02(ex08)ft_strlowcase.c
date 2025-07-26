575d40b308f5bd0291f7fa6ac03f02188a412556
A7TH <122217936+A7meth@users.noreply.github.com>
2025-07-26 13:21:06 +0100

    Update c02(ex08)ft_strlowcase.c

--- c02(ex08)ft_strlowcase.c
+++ c02(ex08)ft_strlowcase.c
@@ -12,13 +12,16 @@
 
 char	*ft_strlowcase(char *str)
 {
+	char	ptr;
+
+	ptr = str;
 	while (*str)
 	{
 		if(*str >= 'A' && *str <= 'Z')
 			*str += 32;
 	*str ++;
 	}
-	return (str);
+	return (ptr);
 }
 
 #include <stdio.h>
