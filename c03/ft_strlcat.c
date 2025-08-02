359ee744089f139592cc5e02e3562d4095fc21ac
A7TH <122217936+A7meth@users.noreply.github.com>
2025-08-02 16:08:12 +0100

    Add files via upload

---
+++ c03/ft_strlcat.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahelamri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/26 21:18:54 by ahelamri          #+#    #+#             */
+/*   Updated: 2025/07/28 10:21:23 by ahelamri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	destl;
+	unsigned int	srcl;
+
+	destl = ft_strlen(dest);
+	srcl = ft_strlen(src);
+	i = 0;
+	if (size <= destl)
+		return (size + srcl);
+	while (src[i] && (destl + i < size -1))
+	{
+		dest[destl + i] = src[i];
+		i++;
+	}
+	dest[destl + i] = '\0';
+	return (destl + srcl);
+}
---
+++ c03/ft_strncat.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahelamri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/26 18:33:27 by ahelamri          #+#    #+#             */
+/*   Updated: 2025/07/28 10:48:06 by ahelamri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	while (dest[i])
+		i++;
+	j = 0;
+	while (src[j] && j < nb)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
